72f2b62dc16614565af49bda7d138af3756bbf70
Matthew Carson <matthew24carson@gmail.com>
2019-05-30 20:51:53 -0400

    Complete simple lolspeak generation

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-const express = require('express');
-const lol = require('lolspeak');
-const txtgen = require('txtgen');
-const app = express();
+const express 		= require('express');
+const lol     		= require('lolspeak');
+const txtgen  		= require('txtgen');
+const bodyParser	= require('body-parser');
+const app     		= express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
 	let text = txtgen.paragraph(5);
 	let loltext = lol(text);
 	res.render('index', { loltext});
+});
+
+app.post('/', (req, res) => {
+	let { paragraphs, sentences } = req.body;
+	
+	let text = '';
+
+	for (let i = 0; i < paragraphs; i++) {
+		text += txtgen.paragraph(req.body.sentences);
+	}
+
+	res.render('index', { 
+		loltext: lol(text),
+		paragraphs,
+		sentences
+	 });
 });
 
 app.listen(3000, () => {
