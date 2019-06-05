9b158af7a42e9dd3c688bd0e0cc3f4586664930f
Matthew Carson <matthew24carson@gmail.com>
2019-06-04 22:10:09 -0400

    Finish design work

--- src/js/components/LolSpeakGenerator.vue
+++ src/js/components/LolSpeakGenerator.vue
@@ -1,20 +1,20 @@
 <template>
-	<div v-cloak class="p-6">
+	<div v-cloak class="px-6 py-3">
 		<div class="flex flex-col w-3/4 mx-auto my-6 items-center">
 			<div class="flex mb-4">
 				<div class="flex flex-col w-1/2 mx-4">
-					<label for="paragraphs">Number of paragraphs</label>
-					<input type="number" v-model="paragraphs" id="paragraphs" value="paragraphs">
+					<label for="paragraphs" class="mb-2">Number of paragraphs</label>
+					<input type="number" v-model="paragraphs" id="paragraphs" value="paragraphs" class="p-2 bg-gray-200 text-lg rounded">
 				</div>
 				<div class="flex flex-col w-1/2 mx-4">
-					<label for="sentences">Number of sentences</label>
-					<input type="number" v-model="sentences" id="sentences" value="sentences">
+					<label for="sentences" class="mb-2">Number of sentences</label>
+					<input type="number" v-model="sentences" id="sentences" value="sentences" class="p-2 bg-gray-200 text-lg rounded">
 				</div>
 			</div>
-			<button @click="getLolSpeak" class="border-solid border-2 border-gray-600 py-6 px-8 rounded hover:bg-gray-600 hover:text-white">Generate</button>
+			<button @click="getLolSpeak" class="border-solid border-2 border-indigo-700 py-6 px-8 rounded hover:bg-indigo-700 text-indigo-700 hover:text-white">Generate</button>
 		</div>
 		<section class="leading-relaxed">
-			<p v-for="paragraph in lolSpeak" v-text="paragraph"></p>
+			<p v-for="paragraph in lolSpeak" v-text="paragraph" class="my-3"></p>
 		</section>
 	</div>
 </template>
